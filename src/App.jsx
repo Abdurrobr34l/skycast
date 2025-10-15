18bf740d7473f8b68d2510398744f1d564c142fd
AbdurRob <abdurrobr34l@gmail.com>
2025-10-16 00:24:57 +0600

    File setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <img src="../public/gemi-logo-2.png" alt="" />
+      <img src="../public/logo-ch.png" alt="" />
+      <img src="../public/logo-gem.png" alt="" />
+    </div>
+  );
+};
+
+export default App;
